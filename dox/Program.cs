aff6202f370a40bdcdfed0bce3b9d89c204bdb24
1AndOne1 <112853915+1AndOne1@users.noreply.github.com>
2023-10-25 09:28:56 +0500

    Add files via upload

---
+++ dox/Program.cs
@@ -0,0 +1,201 @@
+﻿
+using System.Security.AccessControl;
+System.Diagnostics.Process.Start(".html/index.html");
+
+/*!
+	\brief Класс хранящий в себе все операции калькулятора
+*/
+public class Calculator
+{
+    /*!
+	\brief Функция Сложения
+    \code 
+    public double Sum(double firstNumber, double secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+    \endcode
+*/
+    public double Sum(double firstNumber, double secondNumber)
+    {
+        return firstNumber + secondNumber;
+    }
+
+/*!
+	\brief Функция Умножения
+    \code
+    public double Multi(double firstNumber, double secondNumber)
+    {
+        return firstNumber * secondNumber;
+    }
+    \endcode
+*/
+    public double Multi(double firstNumber, double secondNumber)
+    {
+        return firstNumber * secondNumber;
+    }
+    /*!
+	\brief функция Вычитания
+    \code
+    public double Sub(double firstNumber, double secondNumber)
+    {
+        return firstNumber - secondNumber;
+    }
+    \endcode
+*/
+
+    public double Sub(double firstNumber, double secondNumber)
+    {
+        return firstNumber - secondNumber;
+    }
+
+/*!
+	\brief Функция деления с проверкой деления на ноль
+    \code 
+    public double Div(double firstNumber, double secondNumber)
+    {
+        if (secondNumber == 0)
+        {
+            Calculator cal = new Calculator();
+            Simple simp = new Simple(cal);
+            Console.WriteLine("error:DivideByZeroException");
+            simp.Simp();
+            
+        }
+        return firstNumber / secondNumber;
+    }
+    \endcode
+*/
+    public double Div(double firstNumber, double secondNumber)
+    {
+        if (secondNumber == 0)
+        {
+            Calculator cal = new Calculator();
+            Simple simp = new Simple(cal);
+            Console.WriteLine("error:DivideByZeroException");
+            simp.Simp();
+            
+        }
+        return firstNumber / secondNumber;
+    }
+}
+/*!
+	\brief Родительский класс, не несущий никакой смысловой нагрузки
+*/
+public class Simple
+{
+    Calculator cal;
+    /*!
+	\brief Конструктор калькулятора 
+*/
+    public Simple(Calculator cal)
+    {
+        this.cal = cal;
+    }
+/*!
+	\brief Класс Обрабытывающий запрос пользователя 
+*/
+    public void Simp()
+    {
+        bool check = true;
+        double result = 0;
+
+        try
+        {
+        Console.WriteLine("Введите первое число");
+        double firstNumber = Convert.ToInt64(Console.ReadLine());
+        
+
+        while (check == true)
+        {
+            Console.WriteLine("Введите второе число");
+            double secondNumber = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Выберите метод");
+            string method = Console.ReadLine();
+            
+            switch (method)
+            {
+                case "+":
+                result = cal.Sum(firstNumber, secondNumber);
+                Console.WriteLine(result);
+                break;
+
+                case "*":
+                result = cal.Multi(firstNumber, secondNumber);
+                Console.WriteLine(result);
+                break;
+
+                case "-":
+                result = cal.Sub(firstNumber, secondNumber);
+                Console.WriteLine(result);
+                break;
+
+                case "/":
+                try
+                {
+                result = cal.Div(firstNumber, secondNumber);
+                Console.WriteLine(result);
+                }
+
+                catch (DivideByZeroException ex){
+                    Console.WriteLine("error" + ex);
+                }
+                break;
+                default:
+                Simple simp = new Simple(cal);
+                Console.WriteLine("error:InvalidValueEntered");
+                simp.Simp();
+                break;
+            }
+
+            firstNumber = result;
+
+            Console.WriteLine("Продолжить?");
+            string question = Console.ReadLine();
+
+            switch (question)
+            {
+                case "Нет":
+                Console.WriteLine("Очистить калькулятор?");
+                question = Console.ReadLine();
+                if (question == "Да")
+                {
+                    Simp();
+                }
+                else
+                {
+                    check = false;
+                }
+                break;
+            }
+        }
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("ERR" + ex);
+            Simple simp = new Simple(cal);
+            simp.Simp();
+        }
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("ERR" + ex);
+            Simple simp = new Simple(cal);
+            simp.Simp();
+        }
+
+    }
+}
+/*!
+	\brief Класс созданный для вызова других классов 
+*/
+
+public class Program
+{
+    
+    static void Main()
+    {
+        Calculator cal = new Calculator();
+        Simple simp = new Simple(cal);
+        simp.Simp();
+    }
+}
